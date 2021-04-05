8e73030bfb0a26b44f8266967a948c1a39a81654
fronthan <cynthia0330@kakao.com>
2021-04-05 14:00:58 +0900

    working before noon

--- history/pk/src/routes/Login.js
+++ history/pk/src/routes/Login.js
@@ -5,7 +5,16 @@ function Login() {
     return (
         <div className="pk_main">
             <PageTitle h2title="로그인"/>
-             
+            <div className="pk_main pk_login">
+                <div className="box_inp">
+                    <input type="text" className="inp_acnt" placeholder="abc@naver.com"/>
+                </div>
+                <div className="box_inp">
+                    <input type="password" className="inp_pw"/>
+                </div>
+                <button className="btn_login">로그인</button>
+                
+            </div>
         </div>
     )
 }
